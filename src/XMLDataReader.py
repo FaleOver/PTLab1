21db701110231df6f0d4e299010c71f43640e450
FaleOver <mosolova.kate@gmail.com>
2023-11-07 16:39:03 +0300

    Add XMLDataReader and test for it

---
+++ src/XMLDataReader.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import io
+import xml.etree.ElementTree as ET
+from Types import DataType
+from DataReader import DataReader
+
+
+class XMLDataReader(DataReader):
+    def __init__(self) -> None:
+        self.key: str = ""
+        self.students: DataType = {}
+
+    def read(self, path: str) -> DataType:
+        tree = ET.parse(path)
+        root = tree.getroot()
+
+        for child in root:
+            self.key = child.get("name")
+            self.students[self.key] = []
+            for item in child:
+                self.students[self.key].append(
+                    (item.tag, int(item.text)))
+        return self.students
